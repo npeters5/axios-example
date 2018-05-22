3f13a488d4fc60c3cb0fb96d3c7d93356c645ab9
Nora Peters <norapeters5@gmail.com>
2018-05-22 14:04:13 -0700

    adds js for loading pets and handling get request w/no params

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-/* Code goes here */
+// index.js
+const URL = 'https://petdibs.herokuapp.com/pets/';
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
+
+const loadPets = () => {
+  // Prep work
+  const petList = $('#pet-list');
+  petList.empty();
+
+  reportStatus('Loading pets, please wait...');
+
+  // Actually load the pets
+  axios.get(URL)
+    .then((response) => {
+      console.log(response);
+      response.data.forEach((pet) => {
+        petList.append(`<li>${pet.name}</li>`);
+      });
+      reportStatus('Pets loaded.')
+    })
+    .catch((error) => {
+      console.log(error);
+      reportStatus(`Error: ${error.message}`);
+    });
+    console.log('this is after .get');
+    console.log(petList);
+};
+
+$(document).ready(() => {
+  $('#load').click(loadPets);
+});
