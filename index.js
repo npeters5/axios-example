393e6434075cd7d475514446ca3b577b77a5a199
CheezItMan <chris@adadevelopersacademy.org>
2018-05-22 07:52:32 -0700

    get solution

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 
 const URL = 'https://petdibs.herokuapp.com/pets';
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
+
 const loadPets = () => {
+  reportStatus('Loading Pets!');
   const petList = $('#pet-list');
   petList.empty();
 
   axios.get(URL)
     .then((response) => {
+      reportStatus('Successfully loaded Pets');
       response.data.forEach((pet) => {
         petList.append(`<li>${pet.name}</li>`);
       });
     })
     .catch((error) => {
+      reportStatus(`Encountered an error while loading pets ${error.message}`);
       console.log(error);
     });
 }
