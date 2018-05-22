cfd808a97a3a58019099321c70c9540a691e4460
CheezItMan <chris@adadevelopersacademy.org>
2018-05-22 07:48:28 -0700

    load-pets functionality

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+const URL = 'https://petdibs.herokuapp.com/pets';
+
+const loadPets = () => {
+  const petList = $('#pet-list');
+  petList.empty();
+
+  axios.get(URL)
+    .then((response) => {
+      response.data.forEach((pet) => {
+        petList.append(`<li>${pet.name}</li>`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+$(document).ready(() => {
+  $('#load').click(loadPets);
+});
